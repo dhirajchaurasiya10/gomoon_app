30f8f9e59824b1cb46e00ac7e8bc268de1611463
Dhiraj <starkjon224@gmail.com>
2023-10-29 00:00:30 +0545

    added multiple widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,50 +14,76 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-            height: _devicewidth,
-            width: _deviceheight,
+            height: _deviceheight,
+            width: _devicewidth,
             padding: EdgeInsets.symmetric(
               horizontal: _devicewidth * 0.05,
             ),
-            child: _pageTitle()),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _pageTitle(),
+                _destionationDropDownWidget(),
+                _travellersInformationWidget(),
+              ],
+            )),
       ),
     );
   }
-}
 
-Widget _astroImageWidget() {
-  return Container(
-    decoration: const BoxDecoration(
-      // color: Colors.red,
-      image: DecorationImage(
-          fit: BoxFit.contain,
-          image: AssetImage("assets/images/astro_moon.png")),
-    ),
-  );
-}
+  Widget _pageTitle() {
+    //here the _ means it's a private object for a class
+    return const Text(
+      "#GOMoon",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 70,
+        fontWeight: FontWeight.w800,
+      ),
+    );
+  }
 
-Widget _pageTitle() {
-  //here the _ means it's a private object for a class
-  return const Text(
-    "#GOMoon",
-    style: TextStyle(
-      color: Colors.white,
-      fontSize: 70,
-      fontWeight: FontWeight.w800,
-    ),
-  );
-}
+  Widget _astroImageWidget() {
+    return Container(
+      height: _deviceheight * 0.5,
+      width: _devicewidth * 0.65,
+      decoration: const BoxDecoration(
+        // color: Colors.red,
+        image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage("assets/images/astro_moon.png")),
+      ),
+    );
+  }
 
 //jastai devices anusar farak farak notches hunxa ni kasaiko
 //camera bichma kasaiko side ma etc jsle hamro display huni
 //content lai affect garna sakxa so tyo issue lai eradicate garna lai chai safearea le kaam garxa
 
-Widget _destionationDropDownWidget() {
-  return CustomDropDownButtonClass(
-    values: const [
+  Widget _destionationDropDownWidget() {
+    return CustomDropDownButtonClass(values: [
       'James Webb Station',
       'Preneure Station',
-    ],
-    width: _deviceWidth,
-  );
+    ], width: _devicewidth);
+  }
+
+  Widget _travellersInformationWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CustomDropDownButtonClass(
+          values: const ['1', '2', '3', '4'],
+          width: _devicewidth * 0.45,
+        ),
+        CustomDropDownButtonClass(
+          values: const ['Economy', 'Business', 'First', 'Private'],
+          width: _devicewidth * 0.40,
+        ),
+      ],
+    );
+  }
 }
